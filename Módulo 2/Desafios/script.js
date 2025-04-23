15105a473c09a47caa45fc21ec433eeb0833d44e
ericgomesp <ericgomes.aevo@gmail.com>
2025-04-23 13:47:23 -0300

    Initial commit

---
+++ Módulo 2/Desafios/script.js
@@ -0,0 +1,9 @@
+// Exibe uma janela de alerta ao carregar a página
+window.onload = function () {
+    alert("Bem-vindo ao site!");
+};
+
+
+window.onload = function () {
+    window.scrollTo(0, 0);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso-em-v-deo-HTML---CSS
+Curso de HTML+CSS Curso em Vídeo
